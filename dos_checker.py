d5380534b793caa41bc163df060ad554efe48782
Replit user <>
2023-01-12 18:13:09 +0000

    check Cache-Control header in response

--- dos_checker.py
+++ dos_checker.py
@@ -11,6 +11,7 @@ with open(urls_file, "r") as f:
 keyword = "X-Amz-Cf-Pop"
 keyword2 = "X-Amz-Cf-Id"
 keyword3 = "X-Cache"
+keyword4 = "Cache-Control"
 additional_header = {'X-Amz-Server-Side-Encryption': 'AES256xss'}
 
 # Send an HTTP request to each URL and search for the keyword in the response
@@ -19,9 +20,8 @@ for url in urls:
   response = requests.get(url)
   if ((keyword in response.headers) and (keyword2 in response.headers)
       and (keyword3 in response.headers)):
-    additional_response = requests.get(url + '/?test',
-                                       headers=additional_header)
-    if ((additional_response.status_code == 400)):
+    additional_response = requests.get(url + '/?test',headers=additional_header)
+    if ((additional_response.status_code == 400) and (keyword4 in additional_response.headers)):
       print(
         f"Possible Misconfiguration found. Worth checking the URL manually: {url}"
       )
