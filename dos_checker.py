72624ed0f6806be49c1b1634426aa47178bc10a0
Replit user <>
2023-01-12 18:02:40 +0000

    Updated to use splitlines

--- dos_checker.py
+++ dos_checker.py
@@ -2,20 +2,20 @@ import sys
 import requests
 
 # Read the list of URLs from the file specified in the command line argument
+count = 0
 urls_file = sys.argv[1]
 with open(urls_file, "r") as f:
-  urls = f.readlines()
-  count = len(f.readlines())
+  urls = f.read().splitlines()
 
 # The keyword to search for in the HTTP response to find if the website is on Cloudfront
 keyword = "X-Amz-Cf-Pop"
 keyword2 = "X-Amz-Cf-Id"
 keyword3 = "X-Cache"
 additional_header = {'X-Amz-Server-Side-Encryption': 'AES256xss'}
-count = 0
 
 # Send an HTTP request to each URL and search for the keyword in the response
 for url in urls:
+  count = count + 1
   response = requests.get(url)
   if ((keyword in response.headers) and (keyword2 in response.headers)
       and (keyword3 in response.headers)):
